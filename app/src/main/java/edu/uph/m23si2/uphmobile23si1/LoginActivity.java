498ce7571cac6bb68136ef8b094312adf9567a2b
ade-m <66313509+ade-m@users.noreply.github.com>
2025-10-30 11:12:04 +0700

    Realtime Database

--- app/src/main/java/edu/uph/m23si2/uphmobile23si1/LoginActivity.java
+++ app/src/main/java/edu/uph/m23si2/uphmobile23si1/LoginActivity.java
@@ -2,6 +2,7 @@ package edu.uph.m23si2.uphmobile23si1;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,12 +20,18 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
     EditText edtEmail, edtPassword;
-    Button btnLogin;
+    Button btnLogin,btnTambah,btnMerah,btnKuning,btnHijau;
     TextView txvRegister;
     private FirebaseAuth mAuth;
+    Boolean merah,kuning,hijau;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,6 +46,14 @@ public class LoginActivity extends AppCompatActivity {
         edtEmail= findViewById(R.id.edtEmail);
         edtPassword= findViewById(R.id.edtPassword);
         btnLogin= findViewById(R.id.btnLogin);
+        btnTambah= findViewById(R.id.btnTambah);
+
+        btnMerah= findViewById(R.id.btnMerah);
+        btnKuning= findViewById(R.id.btnKuning);
+        btnHijau= findViewById(R.id.btnHijau);
+
+        bacaRD();
+
         txvRegister= findViewById(R.id.txvRegister);
         txvRegister.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,6 +69,80 @@ public class LoginActivity extends AppCompatActivity {
                 String email = edtEmail.getText().toString().trim();
                 String password = edtPassword.getText().toString();
                 login(email,password);
+            }
+        });
+
+        btnTambah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Write a message to the database
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("message");
+
+                myRef.setValue("Hello, World!");
+            }
+        });
+
+        btnMerah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("lalulintas/merah");
+                merah = !merah;
+                myRef.setValue(merah);
+            }
+        });
+        btnHijau.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("lalulintas/hijau");
+                hijau = !hijau;
+                myRef.setValue(hijau);
+            }
+        });
+        btnKuning.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("lalulintas/kuning");
+                kuning = !kuning;
+                myRef.setValue(kuning);
+            }
+        });
+    }
+    private void bacaRD(){
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("lalulintas");
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                merah = dataSnapshot.child("merah").getValue(Boolean.class);
+                kuning = dataSnapshot.child("kuning").getValue(Boolean.class);
+                hijau = dataSnapshot.child("hijau").getValue(Boolean.class);
+
+                if(merah) btnMerah.setText("Matikan Lampu Merah");
+                else  btnMerah.setText("Hidupkan Lampu Merah");
+
+                if(kuning) btnKuning.setText("Matikan Lampu Kuning");
+                else  btnKuning.setText("Hidupkan Lampu Kuning");
+
+                if(hijau) btnHijau.setText("Matikan Lampu Hijau");
+                else  btnHijau.setText("Hidupkan Lampu Hijau");
+
+                Log.d("LampuLaluLintas", "Value is: " + merah);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("LampuLaluLintas", "Failed to read value.", error.toException());
             }
         });
     }
